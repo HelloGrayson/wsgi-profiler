e6ce2aa4d7835250df1b068cea784f2390111b26
Grayson Koonce <grayson@uber.com>
2014-10-24 17:19:51 -0700

    initial version of wsgi-profiler

--- wsgi_profiler/middleware.py
+++ wsgi_profiler/middleware.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 import sys, time, os.path
 try:
     try:
--- wsgi_profiler/util.py
+++ wsgi_profiler/util.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 
 class MergeStream(object):
     """An object that redirects `write` calls to multiple streams.
